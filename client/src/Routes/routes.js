72fa03b6fc55f12c70d3b4b9248a3dc5b9a99e9e
rachidcooder <cooderherou@gmail.com>
2025-05-07 22:47:32 +0100

    edit base url

--- client/src/Routes/routes.js
+++ client/src/Routes/routes.js
@@ -1,5 +1,6 @@
 //const host='https://cabinetmedicalserver.onrender.com/pfe/api'
-const host ='http://44.201.248.88:5000/pfe/api'
+//const host ='http://44.201.248.88:5000/pfe/api'
+const host ='https://cabinet-medical-pfe.vercel.app/pfe/api'
 export const registerRoute=`${host}/register` ;
 export const loginRoute=`${host}/login`;
 
